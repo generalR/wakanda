7f425cca6ac8f0018b0356da397e8223beb0575c
Roy Persson <you@example.com>
2019-02-20 11:35:25 +0100

    uppdaterat telefonnr

--- src/components/Nav/Navigation.js
+++ src/components/Nav/Navigation.js
@@ -58,7 +58,7 @@ class Navigation extends React.Component {
             <div className="mail-info col-xs">
               <span className="mail_adress">E: ulfhelinder@gmail.com</span>{" "}
               <br />
-              <span className="mail_adress">T: +46 735747926</span>
+              <span className="mail_adress">T: +46 706546250</span>
             </div>
           </div>
         </div>
